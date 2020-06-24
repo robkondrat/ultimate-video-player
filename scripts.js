372f8f0eef066c85e77588ff9cc6a597ed865da2
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-06-24 18:01:42 -0500

    pause/play voice commands

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -5,6 +7,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
 
 function togglePlay() {
   if (video.paused) {
@@ -37,10 +40,17 @@ function scrub(e) {
   video.currentTime = scrubTime;
 };
 
+function toggleFullscreen() {
+  video.requestFullscreen();
+};
+
+
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+fullscreen.addEventListener('click', toggleFullscreen);
 
 
 toggle.addEventListener('click', togglePlay);
@@ -54,3 +64,46 @@ progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 
+
+// voice command
+
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+recognition.addEventListener('end', recognition.start);
+
+function voiceControl(transcript) {
+  const playCommand = 'play';
+  if (transcript.includes(playCommand)) {
+    togglePlay();
+  };
+  const pauseCommand = 'pause';
+  if (transcript.includes(pauseCommand)) {
+    togglePlay();
+  };
+  const fullscreenCommand = 'full screen';
+  if (transcript.includes(fullscreenCommand)) {
+    toggleFullscreen();
+  };
+  const skipAheadCommand = 'skip ahead';
+  if (transcript.includes(skipAheadCommand)) {
+    skip();
+  };
+}
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    if(e.results[0].isFinal) {
+      console.log(transcript);
+      voiceControl(transcript);
+    };
+
+
+});
+
+recognition.start();
